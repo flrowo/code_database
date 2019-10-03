f805fa5d981673c17b20c71dfa0ca4bac71bf8c8
João Pedro Libonati <49959057+joaopedrolibonati@users.noreply.github.com>
2019-10-03 05:21:21 +0000

    start a program with priority set to high
    commented = true

---
+++ batch/highPriorityStartup.bat
@@ -0,0 +1,39 @@
+REM this is a comment
+REM this code makes windows to ask for administrator permission
+
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+REM this start the program
+start "" "C:\Users\ryzen2700x\AppData\Local\osu!\osu!.exe"
+REM let's just take a 5 seconds break, to make sure the program will be set to high priority
+timeout /t 5 /nobreak
+REM set the priority to high, using windows management instrumentation command-line (wmic)
+wmic process where name="osu!.exe" CALL setpriority "high"
